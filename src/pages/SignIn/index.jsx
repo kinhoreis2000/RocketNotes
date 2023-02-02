9427081ea381ccee07c42694d2ea9cfa6278efb5
kinhoreis2000 <kinhoreis2000@gmail.com>
2023-02-02 15:41:16 -0300

    commit

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -9,7 +9,7 @@ export function SignIn() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const {signIn} = useAuth()
-
+//função 
   function handleSignIn() {
   
     signIn({email, password})
